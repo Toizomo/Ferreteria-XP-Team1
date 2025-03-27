471ef2aeb34e5b09dc32470de72198269a021d55
Nicolas <jperezmillan8@gmail.com>
2025-03-27 01:38:00 -0500

    Realización del CRUD de Proveedores

--- src/Proveedores/Proveedores.java
+++ src/Proveedores/Proveedores.java
@@ -1,4 +1,37 @@
 package Proveedores;
 
 public class Proveedores {
+    private int id_proveedor;
+    private String nombre;
+    private String contacto;
+
+    public Proveedores(int id_proveedor, String nombre, String contacto) {
+        this.id_proveedor = id_proveedor;
+        this.nombre = nombre;
+        this.contacto = contacto;
+    }
+
+    public int getId_proveedor() {
+        return id_proveedor;
+    }
+
+    public void setId_proveedor(int id_proveedor) {
+        this.id_proveedor = id_proveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
 }
--- src/Proveedores/ProveedoresDAO.java
+++ src/Proveedores/ProveedoresDAO.java
@@ -1,4 +1,104 @@
 package Proveedores;
 
+import Conexion.ConexionBD;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProveedoresDAO {
+
+    ConexionBD ConexionBD = new ConexionBD();
+
+    public void agregar(Proveedores proveedor)
+    {
+
+        Connection con = ConexionBD.getconnection();
+        System.out.println("Nombre: " + proveedor.getNombre() + " | Contacto: " + proveedor.getContacto());
+
+        String query = "INSERT INTO proveedores (nombre, contacto) VALUES (?, ?)";
+
+        try{
+
+            PreparedStatement pst = con.prepareStatement(query);
+
+            pst.setString(1, proveedor.getNombre());
+            pst.setString(2, proveedor.getContacto());
+
+
+            int resultado = pst.executeUpdate();
+
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "Proveedor agregado exitosamente");
+                JOptionPane.showMessageDialog(null, "Depuración: Nombre=" + proveedor.getNombre() + ", Contacto=" + proveedor.getContacto());
+
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Error al agregar Proveedor");
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminar (int id_proveedor)
+    {
+        Connection con = ConexionBD.getconnection();
+
+        String query = "DELETE FROM proveedores WHERE id_proveedor = ?";
+
+        try
+        {
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setInt(1, id_proveedor);
+
+            int resultado = pst.executeUpdate();
+
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "Proveedor eliminado exitosamente");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Error al eliminar proveedor");
+            }
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void actualizar(Proveedores proveedor)
+    {
+
+        Connection con = ConexionBD.getconnection();
+
+        String query = "UPDATE proveedores SET nombre = ?, contacto = ? WHERE id_proveedor = ?";
+
+        try{
+
+            PreparedStatement pst = con.prepareStatement(query);
+
+            pst.setString(1, proveedor.getNombre());
+            pst.setString(2, proveedor.getContacto());
+            pst.setInt(3, proveedor.getId_proveedor());
+
+            int resultado = pst.executeUpdate();
+
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "Proveedor actualizado exitosamente");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Error al actualizar proveedor");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
